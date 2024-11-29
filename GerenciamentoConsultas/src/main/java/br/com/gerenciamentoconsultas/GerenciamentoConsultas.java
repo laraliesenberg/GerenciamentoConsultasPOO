148952d3b4f960cbe37ff99905995111075ef97a
Lara Liesenberg <laraliesenberg@gmail.com>
2024-11-29 19:43:13 -0300

    Criação do projeto

---
+++ GerenciamentoConsultas/src/main/java/br/com/gerenciamentoconsultas/GerenciamentoConsultas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.gerenciamentoconsultas;
+
+/**
+ *
+ * @author laral
+ */
+public class GerenciamentoConsultas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
